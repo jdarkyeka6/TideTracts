2682a79654b4fddb0ea52b847a912a1f9f1ebaef
Jake D <jakedryka1@gmail.com>
2026-09-10 18:05:03 +0800

    Load TideTracts field drag interaction

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./text-field-drag.css";
+import "./text-field-drag.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
